781baec15a5324fd22e618c98a6183cbe3983783
Chris Whinfrey <cwhinfrey@gmail.com>
2018-10-06 16:58:28 -0700

    Finish first test

--- contracts/HarbergerTaxable.sol
+++ contracts/HarbergerTaxable.sol
@@ -14,13 +14,13 @@ contract HarbergerTaxable {
   }
 
   // The total self-assessed value of user's assets
-  mapping(address => uint256) private valueHeld;
+  mapping(address => uint256) public valueHeld;
 
   // Timestamp for the last time taxes were deducted from a user's account
-  mapping(address => uint256) private lastPaidTaxes;
+  mapping(address => uint256) public lastPaidTaxes;
 
   // The amount of ETH a user can withdraw at the last time taxes were deducted from their account
-  mapping(address => uint256) private userBalanceAtLastPaid;
+  mapping(address => uint256) public userBalanceAtLastPaid;
 
   /**
    * Modifiers
@@ -34,7 +34,7 @@ contract HarbergerTaxable {
   /**
    * Public functions
    */
-   
+
   function() public payable {
     userBalanceAtLastPaid[msg.sender] = userBalanceAtLastPaid[msg.sender].add(msg.value);
   }
@@ -42,10 +42,10 @@ contract HarbergerTaxable {
   function withdraw(uint256 value) public {
     // Settle latest taxes
     require(transferTaxes(msg.sender), "User has a negative balance");
-    
+
     // Subtract the withdrawn value from the user's account
     userBalanceAtLastPaid[msg.sender] = userBalanceAtLastPaid[msg.sender].sub(value);
-    
+
     // Transfer remaining balance to msg.sender
     msg.sender.transfer(value);
   }
@@ -53,28 +53,28 @@ contract HarbergerTaxable {
   function userHasPositveBalance(address user) public view returns (bool) {
     return userBalanceAtLastPaid[user] >= _taxesDue(user);
   }
-  
+
   function userBalance(address user) public view returns (uint256) {
     return userBalanceAtLastPaid[user].sub(_taxesDue(user));
   }
-  
+
   // Transfers the taxes a user owes from their account to the taxCollector and resets lastPaidTaxes to now
   function transferTaxes(address user) public returns (bool) {
 
     uint256 taxesDue = _taxesDue(user);
-    
+
     // Make sure the user has enough funds to pay the taxesDue
     if (userBalanceAtLastPaid[user] < taxesDue) {
         return false;
     }
-    
+
     // Transfer taxes due from this contract to the tax collector
     taxCollector.transfer(taxesDue);
     // Update the user's lastPaidTaxes
     lastPaidTaxes[user] = now;
     // subtract the taxes paid from the user's balance
     userBalanceAtLastPaid[user] = userBalanceAtLastPaid[user].sub(taxesDue);
-    
+
     return true;
   }
 
@@ -86,10 +86,12 @@ contract HarbergerTaxable {
   // from their account or since they bought their first token.
   function _taxesDue(address user) internal view returns (uint256) {
     // Make sure user owns tokens
-    require(lastPaidTaxes[user] != 0, "User does not own any tokens");
+    if (lastPaidTaxes[user] == 0) {
+      return 0;
+    }
 
     uint256 timeElapsed = now.sub(lastPaidTaxes[user]);
-    return (valueHeld[user].mul(timeElapsed).div(1 days)).mul(taxPercentage).div(100);
+    return (valueHeld[user].mul(timeElapsed).div(365 days)).mul(taxPercentage).div(100);
   }
 
   function _addToValueHeld(address user, uint256 value) internal {
